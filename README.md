3711608714a114808ef5409f448258886ce13815
Taciano Dreckmann Perez <tdperez@hotmail.com>
2022-12-01 16:35:43 +0000

    added maven archetype

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code-2022
+
+Solutions for [Advent of Code](https://adventofcode.com/2022/).
---
+++ advent-of-code-2022/src/main/java/org/taciano-perez/App.java
@@ -0,0 +1,13 @@
+package org.taciano-perez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
